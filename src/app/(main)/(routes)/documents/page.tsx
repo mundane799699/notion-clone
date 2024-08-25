c45f22be3f9bd5e1d32af65e5e56adfcfabf4a3e
mundane <799699348@qq.com>
2024-08-26 00:27:16 +0800

    Navigation写了一部分，next.js的热更新不行了，是我自己的问题吗？

---
+++ src/app/(main)/(routes)/documents/page.tsx
@@ -0,0 +1,5 @@
+import React, { FC } from "react";
+const DocumentsPage: FC = () => {
+  return <div>documents</div>;
+};
+export default DocumentsPage;
---
+++ src/app/(main)/_components/Navigation.tsx
@@ -0,0 +1,43 @@
+"use client";
+
+import { cn } from "@/lib/utils";
+import { ChevronsLeft } from "lucide-react";
+import { usePathname } from "next/navigation";
+import React, { ElementRef, FC, useRef, useState } from "react";
+import { useMediaQuery } from "usehooks-ts";
+const Navigation: FC = () => {
+  const pathname = usePathname();
+  const isMobile = useMediaQuery("(max-width: 768px)");
+  const isResizingRef = useRef(false);
+  const sidebarRef = useRef<ElementRef<"aside">>(null);
+  const navbarRef = useRef<ElementRef<"div">>(null);
+  const [isResetting, setIsResetting] = useState(false);
+  const [isCollapsed, setIsCollapsed] = useState(isMobile);
+  return (
+    <>
+      <aside
+        ref={sidebarRef}
+        className={cn(
+          "group/sidebar h-full bg-secondary overflow-y-auto relative flex w-60 flex-col z-[99999]",
+          isResetting && "transition-all ease-in-out duration-300",
+          isMobile && "w-0"
+        )}
+      >
+        <div
+          role="button"
+          className="h-6 w-6 text-muted-foreground rounded-sm hover:bg-neutral-300 dark:hover:bg-neutral-600 absolute top-3 right-2 opacity-0 group-hover/sidebar:opacity-100 transition"
+        >
+          <ChevronsLeft className="w-6 h-6" />
+        </div>
+        <div>
+          <p>Action items</p>
+        </div>
+        <div className="mt-4">
+          <p>Documents</p>
+        </div>
+        <div className="opacity-0 group-hover/sidebar:opacity-100 transition cursor-ew-resize absolute h-full w-1 bg-primary/10 right-0 top-0" />
+      </aside>
+    </>
+  );
+};
+export default Navigation;
