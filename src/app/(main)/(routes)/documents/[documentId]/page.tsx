fe26209a5f454e83290c4c0fa374d75419ce1fcf
mundane <799699348@qq.com>
2024-09-09 14:39:17 +0800

    设置emoji图标

--- src/app/(main)/(routes)/documents/[documentId]/page.tsx
+++ src/app/(main)/(routes)/documents/[documentId]/page.tsx
@@ -1,5 +1,39 @@
-const DocumentIdPage = () => {
-  return <div>DocumentIdPage</div>;
+"use client";
+
+import { useQuery } from "convex/react";
+import { api } from "@/../convex/_generated/api";
+import { Id } from "@/../convex/_generated/dataModel";
+import { Toolbar } from "@/components/toolbar";
+
+interface DocumentIdPageProps {
+  params: {
+    documentId: Id<"documents">;
+  };
+}
+
+const DocumentIdPage = ({ params }: DocumentIdPageProps) => {
+  const document = useQuery(api.documents.getById, {
+    documentId: params.documentId,
+  });
+
+  if (document === undefined) {
+    return (
+      <div className="h-full flex items-center justify-center">loading...</div>
+    );
+  }
+
+  if (document === null) {
+    return <div>Not found</div>;
+  }
+
+  return (
+    <div className="pb-40">
+      <div className="h-[35vh]" />
+      <div className="md:max-w-3xl lg:max-w-4xl mx-auto">
+        <Toolbar initialData={document} />
+      </div>
+    </div>
+  );
 };
 
 export default DocumentIdPage;
